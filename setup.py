4f746b2cc52a529062c9ba45150bcabbf266077e
Sanyam Gupta <sanyam23.sg@gmail.com>
2024-02-18 19:36:06 -0800

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'demo math package for testing'
 LONG_DESCRIPTION = 'demo long decription'
 
